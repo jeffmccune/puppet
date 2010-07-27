6c34780cb9a2a008d6dcde2a1864a7ee6cd9232e
Paul Lathrop <paul@simplegeo.com>
2010-07-27 11:12:04 -0700

    Refactor mongo integration.
    * Add search_setup function for initializing mongo support, and remove
      modification to util.rb
    * Pull in latest mongoquery.rb from http://github.com/ripienaar/mcollective-plugins/blob/master/agent/registration-mongodb/puppet/mongoquery.rb

--- lib/puppet/util/mongoquery.rb
+++ lib/puppet/util/mongoquery.rb
@@ -6,11 +6,15 @@ class Puppet::Util::MongoQuery
 
     def initialize
         @connected = false
+
+        @host = nil
+        @db = nil
+        @collection = nil
     end
 
-    # Returns just hostnames matching the query
+    # Returns just fqdn matching the query
     def find_node_names(query)
-        query(query, {:fields => ["hostname"]}).map{|result| result["hostname"]}
+        query(query, {:fields => ["fqdn"]}).map{|result| result["fqdn"]}
     end
 
     # Finds all about nodes matching query
@@ -18,7 +22,36 @@ class Puppet::Util::MongoQuery
         query(query).to_a
     end
 
+    def setup(host, db, collection)
+        unless (host == @host && db == @db && collection == @collection && connected?)
+            @host = host
+            @db = db
+            @collection = collection
+
+            connect
+        end
+    end
+
     private
+    # Connect to mongodb
+    def connect
+        begin
+            # Only reconnect if we see a new set of connection params and we arent
+            # already connected to those
+            unless (@host.nil? || @db.nil? || @collection.nil?)
+                Puppet.notice("Creating new mongo db handle: host=#{@host} db=#{@db} collection=#{@collection}")
+                @dbh = Mongo::Connection.new(@host).db(@db)
+                @coll =@dbh.collection(@collection)
+
+                @connected = true
+            else
+                raise Puppet::ParseError, "Cannot connect to mongo db until setup was called"
+            end
+        rescue Exception => e
+            raise Puppet::ParseError, "Failed to connect to mongo db host=#{@host} db=#{@db} collection=#{@collection}: #{e}"
+        end
+    end
+
     # Checks if we're connected, reconnect if needed
     def query(q, opts={})
         tries = 0
@@ -48,18 +81,4 @@ class Puppet::Util::MongoQuery
             return false
         end
     end
-
-    # Connect to mongodb
-    def connect
-        begin
-            Puppet.notice("Creating new mongo db handle")
-            @dbh = Mongo::Connection.new("localhost").db("puppet")
-            @coll =@dbh.collection("nodes")
-
-            @connected = true
-        rescue Exception => e
-            raise Puppet::ParseError, "Failed to connect to mongo db: #{e}"
-        end
-    end
-
 end
